23cf1092b802d716d3b52408fca2620fcb2e012a
Victor Rubenko <vrubenko@gmail.com>
2022-03-10 01:10:56 +0300

    init

---
+++ SpotifyApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SpotifyApp
+//
+//  Created by Victor Rubenko on 10.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
